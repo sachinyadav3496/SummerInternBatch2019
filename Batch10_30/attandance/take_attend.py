4bf9955c7891a0d254d1f2397bb50947c03d45cf
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-06-11 21:11:55 +0530

    added code for numpy

--- Batch10_30/attandance/take_attend.py
+++ Batch10_30/attandance/take_attend.py
@@ -6,17 +6,18 @@ fp = open("attendance.csv")
 line  = fp.readline().strip('\n')
 
 new_data = line+","+date+'\n'
+c = 1 
 for line in fp : 
     line = line.strip('\n')
     if line : 
-
         name = line.split(',')[0]
-        present = input(f"{name} : ")
+        present = input(f"{c}. {name}  : ")
         if present : 
             present = "A"
         else : 
             present = "P"
         new_data += line+","+present+"\n"
+    c += 1
 else : 
         fp.close()
 
