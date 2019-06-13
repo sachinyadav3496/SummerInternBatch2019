ba6937ac483b989ea72b43803944afcf946ecd40
sachinyadav3496 <sachinyadav3496@gmail.com>
2019-06-13 12:58:26 +0530

    added matplot

---
+++ Batch10_30/data_science/live.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+from matplotlib.animation import FuncAnimation
+#%matplotlib notebook 
+x = list(np.arange(1,11))
+y = list(np.random.randint(10,25,10))
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(x,y,'r^:')
+
+import time
+def change(interval):
+    global x,y
+    x.append(x[-1]+1)
+    y.append(np.random.randint(10,25))
+    time.sleep(interval)
+    ax.clear()
+    ax.plot(x,y,'r^:')
+    
+ani = FuncAnimation(fig,change,1)
+plt.show()
+
